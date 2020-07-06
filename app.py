827594f568b180eb0a8f5dc3172f796faed06ed6
Geulmaster <eyal.geulayev@gmail.com>
2020-07-06 22:31:13 +0300

    Add delete improvements, and details template

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, flash, request, Blueprint 
+from flask import Flask, render_template, flash, request 
 from flask_pymongo import PyMongo
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from utils.handler import read_config
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+app.config["MONGO_URI"] = read_config()
 mongo = PyMongo(app)
 
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+user_collection = mongo.db.users
 
 class Details(Form):
     name = TextField('Name:', validators=[validators.DataRequired()])
@@ -24,15 +27,9 @@ class Details(Form):
             email=request.form['email']
             gender = request.form['options']
 
-            user_collection = mongo.db.users
             user_collection.insert_one({'name' : name, 'email' : email, 'password' : password, 'gender': gender})
             
             print(name, email, password, gender) # In future projects I can jsonify these details
-    
-        if form.validate():
-            flash('Thanks for registration ' + name)
-        else:
-            flash('Error: All the form fields are required. ')
     
         return render_template('index.html', form=form)
     
@@ -42,19 +39,16 @@ def about():
 
 @app.route("/add/<name>")
 def add(name):
-    user_collection = mongo.db.users
     user_collection.insert_one({'name' : str(name), 'email' : 'Python'})
     return '<h1>Added</h1>'
 
 @app.route("/find/<name>")
 def find(name):
-    user_collection = mongo.db.users
     user = user_collection.find_one({'name' : name})
     return f'<h1>User: { user["name"] } <br> Email: { user["email"] }</h1>'
 
 @app.route("/update/<name>/<newName>")
 def update(name, newName):
-    user_collection = mongo.db.users
     user = user_collection.find_one({'name' : name})
     user["name"] = newName
     user_collection.save(user)
@@ -62,19 +56,21 @@ def update(name, newName):
 
 @app.route("/delete/<name>")
 def delete(name):
-    user_collection = mongo.db.users
     user = user_collection.find_one({'name' : name})
-    user_collection.remove(user)
-    return '<h1>Deleted User!</h1>'
+    if user:
+        user_collection.remove(user)
+        return render_template("details.html", msg =  f'User {name} has been deleted successfuly') 
+    else:
+        return render_template("details.html", msg = f'Did not find {name}')
 
 @app.route("/args", methods=['GET'])
 def args():
     """
     Receives ID and returns it
     """
-    poop = request.args.get('id', None)
-    print(poop)
-    return '<h1>{}</h1>'.format(poop)
+    args_method = request.args.get('id', None)
+    print(args_method)
+    return f'<h1>{args_method}</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True)
--- utils/handler.py
+++ utils/handler.py
@@ -1,6 +1,12 @@
+import json
+
 def handle_args(args):
     user = args.get('user')
     if user:
         user = str(args.get('user'))
-
     return user
+
+def read_config():
+    with open('config.json') as config_file:
+        data = json.load(config_file)
+    return data["MONGODB_URL"]
